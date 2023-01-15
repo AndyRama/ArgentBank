f14c5ad0b2c9e9e64ba13e152b772ab32f2051a8
Andy Ramaroson <andyramaroson@gmail.com>
2023-01-15 16:06:52 +0100

    [enh] refacto page SignIn and add style

--- FrontEnd/src/pages/SignIn/SignIn.jsx
+++ FrontEnd/src/pages/SignIn/SignIn.jsx
@@ -1,27 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 import IconUser from '../../component/Icons/IconUser'
 import './SignIn.scss'
 
 function SignIn() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [rememberMe, setRememberMe] = useState(false)
+
+  const handleChangeEmail = (event) => setEmail(event.target.value)
+  const handleChangePassword = (event) => setPassword(event.target.value)
+  const handleChangeRememberMe = () => setRememberMe(!rememberMe)
   return (
-    <section class="sign-in-content">
-      <IconUser />
-      <h1>Sign In</h1>
-      <form>
-        <div class="input-wrapper">
-          <label for="username">Username</label>
-          <input type="text" id="username" />
-        </div>
-        <div class="input-wrapper">
-          <label for="password">Password</label>
-          <input type="password" id="password" />
-        </div>
-        <div class="input-remember">
-          <input type="checkbox" id="remember-me" />
-          <label for="remember-me">Remember me</label>
-        </div>
-      </form>
-    </section>
+    <main className="main sign-in bg-dark">
+      <section className="sign-in-content">
+        <IconUser className="sign-in-icon" />
+        <h1>Sign In</h1>
+        <form action="" id="sign-in-form">
+          <div className="input-wrapper">
+            <label htmlFor="email">Username</label>
+            <input
+              type="text"
+              id="email"
+              autoComplete="username"
+              value={email}
+              onChange={handleChangeEmail}
+            />
+          </div>
+          <div className="input-wrapper">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              value={password}
+              onChange={handleChangePassword}
+            />
+          </div>
+          <div className="input-remember">
+            <input
+              type="checkbox"
+              id="remember-me"
+              onChange={handleChangeRememberMe}
+              checked={rememberMe}
+            />
+            <label htmlFor="remember-me">Remember me</label>
+          </div>
+          <input type="submit" value="Sign In" className="sign-in-button" />
+        </form>
+      </section>
+    </main>
   )
 }
 export default SignIn
