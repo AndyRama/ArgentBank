c7a0e3ec454ac310eee8dd041c2ef222c8c7584b
Andy Ramaroson <andyramaroson@gmail.com>
2023-01-15 11:38:09 +0100

    [enh] add navbar component

--- FrontEnd/src/component/Navbar/Navbar.jsx
+++ FrontEnd/src/component/Navbar/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import logo from '../../asset/argentBankLogo.png'
+import IconUser from '../Icons/IconUser'
+import './Navbar.scss'
 
 function NavBar() {
   return (
@@ -11,6 +13,10 @@ function NavBar() {
           src={logo}
           alt="Argent Bank Logo"
         />
+      </Link>
+      <Link className="main-nav-item" id="SignIn" to="/Login">
+        <IconUser className="sign-in-icon" />
+        <span id="SignIn">Sign In</span>
       </Link>
     </nav>
   )
