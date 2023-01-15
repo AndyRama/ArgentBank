8b0c8dcf3287b286a020e32469633de474214a2f
Andy Ramaroson <andyramaroson@gmail.com>
2023-01-15 15:33:02 +0100

    [enh](feature)create component, add data, add icon and display in home page

--- FrontEnd/src/pages/Home.jsx
+++ FrontEnd/src/pages/Home.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
+import Feature from '../component/Feature/Feature'
+import { featureData } from '../service/data'
 import Hero from '../component/Hero/Hero'
 
 function Home() {
   return (
-    <div>
-      <main className="main-home">
-        <Hero />
-      </main>
-    </div>
+    <main className="main-home">
+      <Hero />
+      {featureData.map((item) => (
+        <Feature
+          key={item.id}
+          src={item.icon}
+          alt={item.alt}
+          text={item.text}
+        />
+      ))}
+    </main>
   )
 }
 
---
+++ FrontEnd/src/service/data.js
@@ -0,0 +1,48 @@
+import chat from '../asset/icon-chat.png'
+import money from '../asset/icon-money.png'
+import security from '../asset/icon-security.png'
+
+export const featureData = [
+  {
+    id: 1,
+    icon: chat,
+    alt: 'chat icon',
+    title: 'You are our #1 priority',
+    text: 'Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes.',
+  },
+  {
+    id: 2,
+    icon: money,
+    alt: 'money icon',
+    title: 'More savings means higher rates',
+    text: 'The more you save with us, the higher your interest rate will be!',
+  },
+  {
+    id: 3,
+    icon: security,
+    alt: 'security icon',
+    title: 'Security you can trust',
+    text: 'We use top of the line encryption to make sure your data and money is always safe.',
+  },
+]
+
+export const accountData = [
+  {
+    id: 1,
+    title: 'Argent Bank Checking (x8349)',
+    amount: '$2,082.79',
+    description: 'Available Balance',
+  },
+  {
+    id: 2,
+    title: 'Argent Bank Savings (x67124)',
+    amount: '$10,928.42',
+    description: 'Available Balance',
+  },
+  {
+    id: 3,
+    title: 'Argent Bank Credit Card (x5201)',
+    amount: '$184.30',
+    description: 'Current Balance',
+  },
+]
